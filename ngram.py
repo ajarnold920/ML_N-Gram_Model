6794ff828021b300433b874779e959c28dff3c49
mjwhite22030 <149093487+mjwhite22030@users.noreply.github.com>
2025-03-03 18:34:03 -0500

    Allow for eval and test sets to be held constant, fix output json

--- ngram.py
+++ ngram.py
@@ -48,13 +48,15 @@ from pygments.lexers.jvm import JavaLexer
 from pygments.lexers import get_lexer_by_name
 from pygments.token import Token
 
-#data = pd.read_csv("ghsajp1.csv", usecols=['Method Java'])
-#data = pd.read_csv("test.csv", usecols=['Method Java'])
 with open(corpus, 'r') as file:
     lines = file.readlines()
-data = pd.DataFrame(lines, columns=['Method Java'])
-#data["Method Java"] = data["Method Java"].replace("\\n","", regex=True)
-#print(len(data))
+train = pd.DataFrame(lines, columns=['Method Java'])
+with open('eval.txt', 'r') as file:
+    lines = file.readlines()
+eval = pd.DataFrame(lines, columns=['Method Java No Comments'])
+with open('test.txt', 'r') as file:
+    lines = file.readlines()
+test = pd.DataFrame(lines, columns=['Method Java No Comments'])
 
 
 
@@ -96,50 +98,23 @@ def remove_comments_from_dataframe(df: pd.DataFrame, method_column: str, languag
     return df
 
 
-# Example usage
-#data = pd.DataFrame({
-#    "Method Java": [
-#        "public void setName(String name) { this.name = name; }",
-#        "public String getName() { return this.name; }",
-#        "public void processData() { System.out.println(\"Processing data\"); }",
-#        "// This is a comment\npublic void processData() { /* Do something */ System.out.println(\"Done\"); }",
-#        "public void doWork() { for(int i=0; i<10; i++) /* Do something */ System.out.println(i); }",
-#    ]
-#})
+print("Initial dataset size:", len(train))
+train = remove_duplicates(train)
+print("After removing duplicates:", len(train))
 
-print("Initial dataset size:", len(data))
-data = remove_duplicates(data)
-print("After removing duplicates:", len(data))
+train = filter_ascii_methods(train)
+print("After filtering ASCII methods:", len(train))
 
-data = filter_ascii_methods(data)
-print("After filtering ASCII methods:", len(data))
+train = remove_outliers(train)
+print("After removing outliers:", len(train))
 
-data = remove_outliers(data)
-print("After removing outliers:", len(data))
+train = remove_boilerplate_methods(train)
+print("After removing boilerplate methods:", len(train))
 
-data = remove_boilerplate_methods(data)
-print("After removing boilerplate methods:", len(data))
+train = remove_comments_from_dataframe(train, "Method Java", "Java")
+print("After cleaning comments:", len(train))
 
-data = remove_comments_from_dataframe(data, "Method Java", "Java")
-print("After cleaning comments:", len(data))
-
-print(data.head())
-
-#data["Method Java No Comments"].to_csv("bum.txt", index=False, header=False)
-#with open('bum.txt', 'r') as infile, open('bum2.txt', 'w') as outfile:
-#    for line in infile:
-#        # Write the line to the output file, skipping the first character
-#        outfile.write(line[1:])
-#with open('bum2.txt', 'r') as infile, open('M_Student.txt', 'w') as outfile:
-#    for line in infile:
-#        # Write the line to the output file, skipping the first character
-#        if(line != "\n"):
-#          outfile.write(line)
-
-from sklearn.model_selection import train_test_split
-
-train_eval, test = train_test_split(data, test_size=0.2, random_state=42)
-train, eval = train_test_split(train_eval, test_size=0.25, random_state=42)
+print(train.head())
 
 #Creates an n-gram model from the given corpus and returns it
 def nGram(corpus, n):
@@ -167,12 +142,22 @@ def nGram(corpus, n):
 
   return model
 
-#model = nGram(train, 3)
-
-#model
-
 vocab = set([])
-for line in data['Method Java No Comments']:
+for line in train['Method Java No Comments']:
+  words = line.split(' ')
+  words = list(filter(None, words))
+  for word in words:
+    if word not in vocab:
+      vocab.add(word)
+
+for line in eval['Method Java No Comments']:
+  words = line.split(' ')
+  words = list(filter(None, words))
+  for word in words:
+    if word not in vocab:
+      vocab.add(word)
+
+for line in test['Method Java No Comments']:
   words = line.split(' ')
   words = list(filter(None, words))
   for word in words:
@@ -200,23 +185,20 @@ def predict(model, sequence):
       prediction = line.correct
   return (prediction, countnum/countdenom)
 
-#testString = '( int'
-#print(predict(model, testString))
 
 import numpy
 
 def perplexity(model, n, eval):
-  perp = numpy.float128(0)
+  perp = 0
   count = 0
   corpus = eval['Method Java No Comments']
-  # print(corpus)
   for line in corpus:
     words = line.split(' ')
     words = list(filter(None, words))
     for i in range(len(words) - n + 1):
       count+=1
-      probnum = numpy.float128(1)
-      probdenom = numpy.float128(vocabLength)
+      probnum = 1
+      probdenom = vocabLength
       gram = ' '.join(words[i:i+n-1])
 
       matches = model[model['gram'] == gram]
@@ -231,21 +213,12 @@ def perplexity(model, n, eval):
           probnum += row.count
 
 
-      # if(probdenom == 0):
-      #   prob = 1
-
-      # prob = numpy.float128(probnum/probdenom)
       prob = numpy.log(probnum/probdenom)
 
       perp += prob
 
-  print(count)
-
   return numpy.exp(-perp/count)
-  # return pow(perp, 1/count)
 
-# Originally got 1586.9922687733
-#perplexity(model, 3, eval)
 
 def iterative_predict(model, seed, functionLength):
   words = [item for item in seed]
@@ -261,8 +234,6 @@ def iterative_predict(model, seed, functionLength):
       predicting = False
   return prediction
 
-#print(iterative_predict(model, ('@', 'Override'), 2))
-
 n = [3, 5, 7]
 models = []
 perps = []
@@ -270,13 +241,11 @@ minPerp = 1000000000
 minPerpIndex = 0
 for i in range(len(n)):
   print("Working on " + str(n[i]))
-  print(corpus[:-4])
   model = nGram(train, n[i])
-  model.to_pickle(corpus[:-4] + "n" + str(n[i]))
+  model.to_pickle(corpus[:-4] + "n" + str(n[i]) + ".pkl")
   models.append(model)
   perp = perplexity(model, n[i], eval)
   perps.append(perp)
-  print("perp")
   print(str(n[i]) + "-Gram Perplexity: " + str(perp))
   if(perp < minPerp):
     minPerp = perp
@@ -299,26 +268,11 @@ for i in range(100):
     method = test['Method Java No Comments'].iloc[rand]
   method = list(filter(None, method.split(' ')))
   seed = tuple(method[:n[minPerpIndex]-1])
-  print(seed)
   prediction = iterative_predict(model, seed, len(method))
   dictionary = {
     "index": str(i),
     "prediction": prediction,
   }
-  with open("sample.json", "a") as outfile:
+  with open("results" + corpus[1:-4] + "_model" + ".json", "a") as outfile:
     json.dump(dictionary, outfile)
 
-#Train models (3, 5, 7, 9) maybe more using ghs data
-#Calculate perplexity on evaluation set
-#Test models on test set and make predictions
-
-#Train new models on his data
-#Calculate perplexity on SAME evaluation set from the ghs data
-#Test models on SAME test set from ghs data make predictions
-
-#save models with pkl
-#60-80% in training set
-
-#Model keeps predicting until parenthesis are balanced or probabilities very close to 0
-#Predict from the first n of the tokens
-#If we want, we can "cheat" and use the length of the method and generate up to that
